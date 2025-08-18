7d559519b0c2091d3787f366bdefdb03cd3d0986
JoshNat426 <joshua.natanael2003@gmail.com>
2025-08-18 03:46:08 +0000

    updated model size in input

--- src/app/ml/model_io.py
+++ src/app/ml/model_io.py
@@ -7,7 +7,7 @@ from app.ml.config import MODEL_PATH, SCALER_PATH, DEVICE
 from app.ml.lstm_arch import LSTMModel
 
 def load_model_and_scaler(features_len: int):
-    model = LSTMModel(input_size=features_len, hidden_sizes=[200, 100, 50], dropout=0.3).to(DEVICE)
+    model = LSTMModel(input_size=features_len, hidden_sizes=[256, 128], dropout=0.2).to(DEVICE)
     model.load_state_dict(torch.load(MODEL_PATH, map_location=DEVICE))
     model.eval()
     scaler = joblib.load(SCALER_PATH)
